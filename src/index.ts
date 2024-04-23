bad1c1dfb20039298cda98cbb19ea7bf475674c1
Carlos Pinho <cpinho@imaginarycloud.com>
2024-04-23 10:10:23 +0100

    Adds email template annd extended options

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,19 @@
 import { RedisCacheService, type RedisCacheServiceOptions } from './lib/redis-cache-service';
 
-import { NodemailerDeliveryService, type NodemailerDeliveryServiceOptions } from './lib/email-delivery-service';
+import { NodemailerDeliveryService, type NodemailerDeliveryServiceOptions, type MailOptions } from './lib/email-delivery-service';
 import { OTPAuthentication } from './lib/otp-authentication';
 
 import { generate } from 'otp-generator';
 
 export class EmailOTPAuthentication extends OTPAuthentication {
-    constructor(redisOptions: RedisCacheServiceOptions, nodemailerOptions: NodemailerDeliveryServiceOptions) {
+    constructor(
+        redisOptions: RedisCacheServiceOptions,
+        deliveryServiceOptions: { nodemailerOptions: NodemailerDeliveryServiceOptions; mailOptions: MailOptions },
+    ) {
         super(
             () => generate(6, { lowerCaseAlphabets: false, specialChars: false }),
             new RedisCacheService(redisOptions),
-            new NodemailerDeliveryService(nodemailerOptions),
+            new NodemailerDeliveryService(deliveryServiceOptions.nodemailerOptions, deliveryServiceOptions.mailOptions),
         );
     }
 }
